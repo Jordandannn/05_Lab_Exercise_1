db1d26fc10bdf452cf103c9bc9ac78132b235b3c
perez.277243 <perez.277243@LAB-C-PC20>
2023-11-13 19:08:08 +0800

    Add project files.

---
+++ Creating Text File/FrmFileName.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Creating_Text_File
+{
+    partial class FrmFileName
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOkay = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOkay
+            // 
+            this.btnOkay.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnOkay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOkay.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOkay.Location = new System.Drawing.Point(73, 119);
+            this.btnOkay.Name = "btnOkay";
+            this.btnOkay.Size = new System.Drawing.Size(75, 33);
+            this.btnOkay.TabIndex = 3;
+            this.btnOkay.Text = "Okay";
+            this.btnOkay.UseVisualStyleBackColor = false;
+            this.btnOkay.Click += new System.EventHandler(this.btnOkay_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFileName.Location = new System.Drawing.Point(38, 58);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(158, 29);
+            this.txtFileName.TabIndex = 2;
+            // 
+            // FrmFileName
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(234, 211);
+            this.Controls.Add(this.btnOkay);
+            this.Controls.Add(this.txtFileName);
+            this.Name = "FrmFileName";
+            this.Text = "FrmFileName";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtFileName;
+        public System.Windows.Forms.Button btnOkay;
+    }
+}
